4ab458e18cbf841e0540ed39e453ec20be5999bf
Francis Inegbenose <62514538+Fransco35@users.noreply.github.com>
2023-06-21 11:26:39 +0100

    successfully created and designed the endpoints for storing, completing and deleting tasks in mongodb database

--- index.js
+++ index.js
@@ -41,11 +41,40 @@ app.post("/api/addTasks", (req, res) => {
       res.status(201).json({ message: "Task created successfully" });
     })
     .catch((error) => {
-      console.log(error.message);
+      res.send(error.message);
     });
+});
+
+app.patch("/api/task/:id", async (req, res) => {
+  const requestedTaskId = req.params.id;
+
+  const ObjectId = require("mongodb").ObjectId;
+
+  try {
+    await Task.updateOne(
+      { _id: new ObjectId(requestedTaskId) },
+      { $set: { status: "complete" } }
+    );
+    res.send({ message: "Task updated successfully" });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+app.delete("/api/task/:id", async (req, res) => {
+  const requestedTaskId = req.params.id;
+  const ObjectId = require("mongodb").ObjectId;
+
+  try {
+    await Task.deleteOne({ _id: new ObjectId(requestedTaskId) });
+    res.send({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
+
